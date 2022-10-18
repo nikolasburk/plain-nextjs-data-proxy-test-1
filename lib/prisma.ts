3be68a16233a05c32288b72ad6bfef60e4b5f0f9
Nikolas Burk <nikolas.burk@gmail.com>
2022-10-18 10:40:23 +0200

    remove query logs

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -11,7 +11,7 @@ declare global {
 }
 
 const prisma = global.prisma || new PrismaClient({
-  log: ['error', 'info', 'query']
+  log: ['error', 'info']
 })
 
 if (process.env.NODE_ENV !== 'production') global.prisma = prisma
